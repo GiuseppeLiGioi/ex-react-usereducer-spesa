94b6a14cd1227f6428c9c9c35704485c3a412d39
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-05-23 11:38:44 +0200

    added. milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
-import { useState } from 'react'
-import './App.css'
+/*
+📌 Milestone 1: Mostrare la lista dei prodotti
+1. Parti dall’array products fornito:
+const products = [
+  { name: 'Mela', price: 0.5 },
+  { name: 'Pane', price: 1.2 },
+  { name: 'Latte', price: 1.0 },
+  { name: 'Pasta', price: 0.7 },
+];
+Crea un componente che mostra la lista dei prodotti.
+Per ogni prodotto, mostra:
+Nome
+Prezzo
+
+Obiettivo: Vedere un elenco leggibile di tutti i prodotti con nome e prezzo.
+*/
+
+
+
+
+const products = [
+  { name: 'Mela', price: 0.5 },
+  { name: 'Pane', price: 1.2 },
+  { name: 'Latte', price: 1.0 },
+  { name: 'Pasta', price: 0.7 },
+];
 
 function App() {
-  
+
 
   return (
     <>
+    <h2>LISTA DELLA SPESA</h2>
+      <ul>
 
+      
+        {
+           products.map((p, index) => (
+            
+              <li key={index}>{p.name}{p.price}</li>
+              
+            
+           ))
+        }
+      </ul>
     </>
   )
 }
